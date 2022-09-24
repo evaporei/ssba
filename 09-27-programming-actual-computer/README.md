5c2b54dde5840b355085a16e4851bc184902e333
Eva Pace <eba.pachi@gmail.com>
2022-09-24 15:13:36 -0300

    programming-actual-computer: improve mac m1 instructions

--- 09-27-programming-actual-computer/README.md
+++ 09-27-programming-actual-computer/README.md
@@ -25,7 +25,7 @@ xcrun -sdk macosx --show-sdk-path # /Applications/Xcode.app/Contents/Developer/P
 
 as -o hello_m1.o hello_m1.asm
 
-ld -macosx_version_min 12.0.0 -o hello_m1 hello_m1.o -lSystem -syslibroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX12.3.sdk -e _start -arch arm64
+ld -macosx_version_min 12.0.0 -o hello_m1 hello_m1.o -lSystem -syslibroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX12.3.sdk -e _main -arch arm64
 
 ./hello_m1
 ```
--- 09-27-programming-actual-computer/hello_m1.asm
+++ 09-27-programming-actual-computer/hello_m1.asm
@@ -1,27 +1,37 @@
-//
-// Assembler program to print "Hello World!"
-// to stdout.
-//
-// X0-X2 - parameters to linux function services
-// X16 - linux function number
-//
-.global _start             // Provide program starting address to linker
+; ----------------------------------------------------------------------------------------
+; Writes "Hello, World" to the console using only system calls. Runs on 64-bit m1 macOS only.
+; To assemble and run:
+;
+;     xcrun -sdk macosx --show-sdk-path # /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX12.3.sdk
+
+;     as -o hello_m1.o hello_m1.asm
+
+;     ld -macosx_version_min 12.0.0 -o hello_m1 hello_m1.o -lSystem -syslibroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX12.3.sdk -e _main -arch arm64
+
+;     ./hello_m1
+;
+; Derived from:
+;   - https://cs.lmu.edu/~ray/notes/nasmtutorial/
+;   - https://smist08.wordpress.com/2021/01/08/apple-m1-assembly-language-hello-world/
+; ----------------------------------------------------------------------------------------
+
+.global _main          ; provide program starting address to linker
 .align 2
 
-// Setup the parameters to print hello world
-// and then call Linux to do it.
+; setup the parameters to print hello world
+; and then call the OS to do it.
 
-_start: mov X0, #1     // 1 = StdOut
-        adr X1, helloworld // string to print
-        mov X2, #13     // length of our string
-        mov X16, #4     // MacOS write system call
-        svc 0     // Call linux to output the string
+_main: mov X0, #1      ; file handle 1 is stdout
+       adr X1, message ; address of string to print
+       mov X2, #13     ; length of our string, number of bytes
+       mov X16, #4     ; MacOS write system call
+       svc 0           ; invoke operating system to do the write
 
-// Setup the parameters to exit the program
-// and then call Linux to do it.
+; setup the parameters to exit the program
+; and then call the OS to do it.
 
-        mov     X0, #0      // Use 0 return code
-        mov     X16, #1     // Service command code 1 terminates this program
-        svc     0           // Call MacOS to terminate the program
+       mov X0, #0      ; exit code 0
+       mov X16, #1     ; MacOS exit system call (service command code 1 terminates this program)
+       svc 0           ; invoke operating system to exit
 
-helloworld:      .ascii  "Hello World!\n"
+message:      .ascii  "Hello World!\n"
