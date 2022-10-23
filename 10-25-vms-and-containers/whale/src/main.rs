55dc3d100674e6c56c926a392f63b3a6ae75d2f6
Eva Pace <eba.pachi@gmail.com>
2022-10-23 15:09:17 -0300

    vms-and-containers: clap bootstrap

--- 10-25-vms-and-containers/whale/src/main.rs
+++ 10-25-vms-and-containers/whale/src/main.rs
@@ -1,3 +1,28 @@
+use clap::{Arg, ArgAction, Command};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = Command::new("whale")
+        .about("container runtime")
+        .version("0.1.0")
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .author("Eva Pace")
+        .subcommand(
+            Command::new("create")
+                .about("create container")
+                .arg(
+                    Arg::new("bundle").required(true), // .action(ArgAction::Set)
+                                                       // .num_args(1..),
+                )
+                .arg(Arg::new("id").required(true)),
+        )
+        .subcommand(Command::new("start").arg(Arg::new("id").required(true)))
+        .subcommand(
+            Command::new("kill")
+                .arg(Arg::new("id").required(true))
+                .arg(Arg::new("signal").required(true)),
+        )
+        .subcommand(Command::new("delete").arg(Arg::new("id").required(true)))
+        .subcommand(Command::new("state").arg(Arg::new("id").required(true)))
+        .get_matches();
 }
